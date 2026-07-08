0892a18c173cee9a8266662172c500fb14f18bfc
Praveen <praveenradharanga@gmail.com>
2026-07-08 20:55:21 +0000

    nodeport

--- roadmap/rm.sh
+++ roadmap/rm.sh
@@ -8,3 +8,22 @@ Terraform  → later
 Ansible    → comes with CI/CD stage
 
 Date: 06/06/2026
+----------------------------
+
+Linux  ✅
+    ↓
+Git    ✅
+    ↓
+Docker ✅
+    ↓
+Kubernetes (Minikube) ← Continue here
+    ↓
+AWS (EC2, IAM, VPC, S3)
+    ↓
+EKS
+    ↓
+Terraform
+    ↓
+GitHub Actions / Jenkins
+    ↓
+Monitoring (Prometheus + Grafana)
